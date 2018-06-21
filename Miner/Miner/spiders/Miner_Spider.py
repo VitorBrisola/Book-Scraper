4b4f569eda516d9c20215f2956119dbe5dbc6b41
Vitor Brisola <vitoraugusto199@gmail.com>
2018-06-21 08:28:31 -0300

    version-0.2- extracting and listing urls

--- Miner/Miner/spiders/Miner_Spider.py
+++ Miner/Miner/spiders/Miner_Spider.py
@@ -12,16 +12,16 @@ class MinerSpider(scrapy.Spider):
     def parse(self, response):
 
         # Extracting page title
-        yield{ 'title' : ''.join(response.css('div#block-st-zen-page-title>h1>span::text').extract())
+        yield{ 'title' : ''.join(response.css('title::text').extract_first()),
+                #'urls' : ' '.join(response.css('li a::attr(href)').extract()), #gets all the urls in the page
+                'NextPage' : ' '.join(response.css('em a::attr(href)').extract()), #gets the next page url
                 }
 
-         # Extracting data
-        #for quote in response.css('div.quote'):
-            # making a dictionary with the data
-        #    yield { 
-        #        'text': quote.css('span.text::text').extract_first(),
-        #        'author': quote.css('small.author::text').extract_first(),
-        #        'tags': quote.css('div.tags a.tag::text').extract(),
-        #    }
+        # preciso fazer com que liste a resquisicao de todas as urls extraidas
+        # pois na segunda pagina ao extrair so a primeira url ele volta para a inicial 
+        next_page = response.css('em a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
         pass
