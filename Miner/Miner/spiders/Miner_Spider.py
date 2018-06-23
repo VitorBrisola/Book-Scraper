7d1762fffdda38cfc889db136892e82bbf6b1f3d
Vitor Brisola <vitoraugusto199@gmail.com>
2018-06-23 09:54:31 -0300

    Scraping 3 pages from the start url

--- Miner/Miner/spiders/Miner_Spider.py
+++ Miner/Miner/spiders/Miner_Spider.py
@@ -12,16 +12,22 @@ class MinerSpider(scrapy.Spider):
     def parse(self, response):
 
         # Extracting page title
-        yield{ 'title' : ''.join(response.css('title::text').extract_first()),
-                #'urls' : ' '.join(response.css('li a::attr(href)').extract()), #gets all the urls in the page
-                'NextPage' : ' '.join(response.css('em a::attr(href)').extract()), #gets the next page url
-                }
+        yield{ 
+            'title' : response.css('title::text').extract_first(),
+            #'urls' : ' '.join(response.css('p a::attr(href)').extract()), #gets all the urls in the page
+            'NextPage' : response.css('p em a::attr(href)' or 'p a em::attr(href)').extract(), #gets the next page url
+        }
 
         # preciso fazer com que liste a resquisicao de todas as urls extraidas
         # pois na segunda pagina ao extrair so a primeira url ele volta para a inicial 
-        next_page = response.css('em a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        #next_page = response.css('em a::attr(href)').extract_first()
+        #if next_page is not None:
+        #    next_page = response.urljoin(next_page)
+        #    yield scrapy.Request(next_page, callback=self.parse)
 
-        pass
+        # o html muda e nao consegue pegar o cap 4 pq a ordem de 'a em' para 'em a' ,tem que pegar os dois
+        for a in (response.css('em a')):
+            yield response.follow(a, callback=self.parse)
+           
+
+        #pass
